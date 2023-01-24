a94d8c17e3c8a42ee32ad22ca1c18fdc91ae341c
Fabricio <fabricio.caceres@hotmail.com>
2023-01-24 18:22:55 -0300

    :construction: Adicionado cabeçalho e rodapé

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,12 @@
+import styles from './Rodape.module.css';
+
+function Rodape() {
+    return (
+        <footer className={styles.rodape}>
+            <h2>Desenvolvido por Binho </h2>
+            <h3>(Ah vá)</h3>
+        </footer>
+    )
+}
+
+export default Rodape;
