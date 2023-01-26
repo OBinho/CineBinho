e65c1ab5ba411494eb96678ba094469bf58c51a6
Fabricio <fabricio.caceres@hotmail.com>
2023-01-26 17:28:14 -0300

    :beers: Tentando entender context, socorro :(

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -2,8 +2,11 @@ import styles from './Favoritos.module.css';
 import Banner from 'components/Banner';
 import Card from 'components/Card';
 import Titulo from 'components/Titulo';
+import { useFavoritoContext } from 'contextos/Favoritos';
 
 function Favoritos() {
+    const { favorito } = useFavoritoContext();
+
     return (
         <>
             <Banner imagem={'favoritos'} />
@@ -11,7 +14,9 @@ function Favoritos() {
                 <h1>Seus Favoritos</h1>
             </Titulo>
             <section className={styles.container}>
-                <Card id='1' titulo='Daniel Bobaião' capa='https://clips-media-assets2.twitch.tv/asNvehGLD-5OxmR4QbqXLg/vod-1705596251-offset-7250-preview-260x147.jpg' />
+                {favorito.map((fav) => {
+                    return <Card {...fav} key={fav.id} />
+                })}
             </section>
         </>
     )
